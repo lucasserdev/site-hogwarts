5338b44455e52e5b34c266fd2c5de017eb62e3f4
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2024-04-03 23:27:00 -0300

    sim

---
+++ reveal.js
@@ -0,0 +1,76 @@
+window.revelar = ScrollReveal({reset:true})
+
+// TOPO DO SITE
+
+revelar.reveal('.efeito-txt-topo', 
+{
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', 
+{
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+
+revelar.reveal('.result1', 
+{
+    duration: 2000,
+    distance: '100px',
+})
+
+revelar.reveal('.choose-title', 
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 500
+})
+
+revelar.reveal('.efect1', 
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 1000
+})
+
+revelar.reveal('.efect2', 
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 1500
+})
+
+revelar.reveal('.efect3', 
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 2000
+})
+
+revelar.reveal('.efect4', 
+{
+    duration: 2000,
+    distance: '100px',
+    delay: 2500
+})
+
+revelar.reveal('.letter-text', 
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'right',
+    delay: 500
+})
+
+revelar.reveal('.letter-img', 
+{
+    duration: 2000,
+    distance: '100px',
+    origin: 'left',
+    delay: 300
+})
+
+
